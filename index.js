ea02204bad8f8067f5efa586993852445cfa5b40
garychen <garychen@pptv.com>
2017-02-27 16:47:13 +0800

    file

--- index.js
+++ index.js
@@ -3,89 +3,93 @@ var qs = require('querystring');
 var co = require('co');
 var prompt = require('co-prompt');
 var exec = require('child_process').exec;
+var opn = require('opn');
 
-co(function*(){
-	var branch = yield prompt('branch name: ');
-	var path = yield prompt('release path: ' );
-	var note = yield prompt('remarks: ');
-	var user = yield prompt('user: ');
 
-	//post 发送
-	var content = qs.stringify({
-		json: JSON.stringify({
-			"parameter": [
-				{ "name": "branchname", "value": branch },
-				{ "name": "release_path", "value": path },
-				{ "name": "Remarks", "value": note },
-				{ "name": "qidongyonghu", "value": user }
-			],
-			"statusCode": "303",
-			"redirectTo": "."
-		}),
-		time: new Date().getTime()
-	});
-	var options = {
-		host: 'jenkins',
-		port: 80,
-		path: '/view/StaticResource_Release/job/staticdeploy_sports/build?delay=0sec',
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-			'Content-Length': content.length
-		}
-	};
+// co(function*(){
+// 	var branch = yield prompt('branch name: ');
+// 	var path = yield prompt('release path: ' );
+// 	var note = yield prompt('remarks: ');
+// 	var user = yield prompt('user: ');
 
-	var req = http.request(options, function(res){
-		var _data = '';
+// 	//post 发送
+// 	var content = qs.stringify({
+// 		json: JSON.stringify({
+// 			"parameter": [
+// 				{ "name": "branchname", "value": branch },
+// 				{ "name": "release_path", "value": path },
+// 				{ "name": "Remarks", "value": note },
+// 				{ "name": "qidongyonghu", "value": user }
+// 			],
+// 			"statusCode": "303",
+// 			"redirectTo": "."
+// 		}),
+// 		time: new Date().getTime()
+// 	});
+// 	var options = {
+// 		host: 'jenkins',
+// 		port: 80,
+// 		path: '/view/StaticResource_Release/job/staticdeploy_sports/build?delay=0sec',
+// 		method: 'POST',
+// 		headers: {
+// 			'Content-Type': 'application/x-www-form-urlencoded',
+// 			'Content-Length': content.length
+// 		}
+// 	};
 
-		res.on('data', function(chunk){
-			_data += chunk;
-		});
+// 	var req = http.request(options, function(res){
+// 		var _data = '';
 
-		res.on('end', function(){
-			// console.log( "\n--->>\nresult:", _data );
-		});
-	});
-	req.write(content);
-	req.end();
-	console.log( '\n' );
-	console.log('发版成功，正在获取版本号，请稍后...');
+// 		res.on('data', function(chunk){
+// 			_data += chunk;
+// 		});
+
+// 		res.on('end', function(){
+// 			// console.log( "\n--->>\nresult:", _data );
+// 		});
+// 	});
+// 	req.write(content);
+// 	req.end();
+// 	console.log( '\n' );
+// 	console.log('发版中, 请稍后......');
 	
 
 
-	//get version
-	setTimeout(()=>{
-		http.get({
-			host: 'jenkins',
-			port: 80,
-			path: '/view/StaticResource_Release/job/staticdeploy_sports/lastSuccessfulBuild/consoleText',
-			method: 'GET',
-			headers: {
-			    'Content-Type': 'application/json'
-			}
-		}, function(res){
-		    var resData = '';
+// 	//get version
+// 	setTimeout(()=>{
+// 		http.get({
+// 			host: 'jenkins',
+// 			port: 80,
+// 			path: '/view/StaticResource_Release/job/staticdeploy_sports/lastSuccessfulBuild/consoleText',
+// 			method: 'GET',
+// 			headers: {
+// 			    'Content-Type': 'application/json'
+// 			}
+// 		}, function(res){
+// 		    var resData = '';
 
-		    res.on('data', function(data){
-		        resData += data;
-		        // console.log(data);
-		    });
+// 		    res.on('data', function(data){
+// 		        resData += data;
+// 		        // console.log(data);
+// 		    });
 
-		    res.on('end', function() {
-		        callback( resData );
-		    });
-		});
-		function callback( data ){
-			var ver = data.match(/VERSION=v_\d+/)[0].replace('VERSION=', '');
+// 		    res.on('end', function() {
+// 		        callback( resData );
+// 		    });
+// 		});
+
+// 		function callback( data ){
+// 			var ver = data.match(/VERSION=v_\d+/)[0].replace('VERSION=', '');
 			
-			exec('echo '+ ver +' | clip');
-			console.log( '版本号: ', ver, '(已粘贴到剪切板)' );
-		}
-	}, 1000 * 60 * 1);
+// 			exec('echo '+ ver +' | clip');
+// 			console.log( '发版成功, 版本号: ', ver, ' (已粘贴到剪切板)' );
+// 			opn('http://jenkins/view/StaticResource_Release/job/staticdeploy_sports/');
+// 		}
+// 	}, 1000 * 60 * 1);
 
 
 
-	process.stdin.pause();
-});
+// 	process.stdin.pause();
+// });
 
 
